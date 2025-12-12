ac32fac288361fd077feedcb5bde09c4dcec33fe
Viviana <40200339@esmad.ipp.pt>
2025-12-12 07:40:36 +0000

    squares that have already been clicked on can't get clicked again

--- ticTacToe.js
+++ ticTacToe.js
@@ -8,16 +8,31 @@ let oddSquares = document.querySelectorAll(".odd")
 let evenSquares = document.querySelectorAll(".even")
 let turn = "x";
 
-// function for any square clicked
+/**
+ * for any square clicked
+ * @param {*} row row of the square clicked
+ * @param {*} square if it was the 1st, 2nd or 3rd square
+ * @param {*} e event
+ */
 function square(row, square, e) {
-    if (turn == "x") {
-        e.target.innerHTML = `x`;
-        savePos(row, square, turn)
-        turn = "o"
+    if (isSquareFree(row, square, e)) { 
+        if (turn == "x") {
+            e.target.innerHTML = `x`;
+            savePos(row, square, turn)
+            turn = "o"
+        } else {
+            e.target.innerHTML = `o`;
+            savePos(row, square, turn)
+            turn = "x"
+        }
+    }
+}
+
+function isSquareFree(row, square, e) {
+    if (e.target.innerHTML == "x" || e.target.innerHTML == "o") {
+        return false;
     } else {
-        e.target.innerHTML = `o`;
-        savePos(row, square, turn)
-        turn = "x"
+        return true;
     }
 }
 
