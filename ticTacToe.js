624a5c71a4e0ca1938cd76ea878a61e7a94f2cfc
Viviana <40200339@esmad.ipp.pt>
2025-12-12 07:33:10 +0000

    changed from event listenners to event handlers.
    saved the position of the square clicked in an array

--- ticTacToe.js
+++ ticTacToe.js
@@ -8,33 +8,20 @@ let oddSquares = document.querySelectorAll(".odd")
 let evenSquares = document.querySelectorAll(".even")
 let turn = "x";
 
-// detects click in any of the odd squares
-for (const square of oddSquares) {
-    square.addEventListener("click", () => {
-        if (turn == "x") {
-            square.innerHTML = `x`;
-            turn = "o"
-        } else {
-            square.innerHTML = `o`;
-            turn = "x"
-        }
-
-        console.log(square.parentNode.id);
-    })
+// function for any square clicked
+function square(row, square, e) {
+    if (turn == "x") {
+        e.target.innerHTML = `x`;
+        savePos(row, square, turn)
+        turn = "o"
+    } else {
+        e.target.innerHTML = `o`;
+        savePos(row, square, turn)
+        turn = "x"
+    }
 }
 
-// detects click in any of the even squares
-for (const square of evenSquares) {
-    square.addEventListener("click", () => {
-        if (turn == "x") {
-            square.innerHTML = `x`;
-            turn = "o"
-        } else {
-            square.innerHTML = `o`;
-            turn = "x"
-        }
-
-        console.log(square.parentNode.id);
-    })
+function savePos(row, square) {
+    game[row-1][square-1] = turn;
+    console.log(game);
 }
-
